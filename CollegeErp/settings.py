2c0fec7ad8c7c2008705c88b67ad37632bde053e
kawserraihan <kawserraihan2@gmail.com>
2024-01-18 23:20:49 +0600

    testing news images with debug true

--- CollegeErp/settings.py
+++ CollegeErp/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-a_hperyi!b4y##l+ymm!6gr(3k^8b4(q#^)nkv8)q^3bz(5^fl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
